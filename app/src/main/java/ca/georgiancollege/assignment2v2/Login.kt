cc971f8ed1a44db7949d9aff12cfc7b4ceadfb6e
mackintac <mack-robinson@hotmail.ca>
2025-08-05 14:16:23 -0400

    add firebase auth to Register

--- app/src/main/java/ca/georgiancollege/assignment2v2/Login.kt
+++ app/src/main/java/ca/georgiancollege/assignment2v2/Login.kt
@@ -1,11 +1,14 @@
 package ca.georgiancollege.assignment2v2
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import ca.georgiancollege.assignment2v2.databinding.ActivityLoginBinding
+import ca.georgiancollege.assignment2v2.databinding.ActivityRegisterBinding
 
 class Login : AppCompatActivity() {
 
@@ -20,6 +23,11 @@ class Login : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.registerNowTextView.setOnClickListener {
+                val intentObj = Intent(applicationContext, Register::class.java)
+                startActivity(intentObj)
         }
     }
 }
--- app/src/main/java/ca/georgiancollege/assignment2v2/Register.kt
+++ app/src/main/java/ca/georgiancollege/assignment2v2/Register.kt
@@ -6,9 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import ca.georgiancollege.assignment2v2.databinding.ActivityRegisterBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class Register : AppCompatActivity() {
-
+    private lateinit var auth: FirebaseAuth;
     private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +24,13 @@ class Register : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        auth = Firebase.auth
+
+
+    }
+
+    fun registerUser(email:String, password:String){
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(
+            OnCompleteListener {  })
     }
 }
